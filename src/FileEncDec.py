d6349072578f613c4fefd79adb4a3dca5a5c4a9c
jay <jay@2c8e1322-2329-47d6-85ce-3116e258dea3>
2007-03-13 17:33:44 +0000

    Initial, finished, revision of the FileEncDec class
    git-svn-id: https://svn.splitstreams.com:444/pwenc/trunk@13 2c8e1322-2329-47d6-85ce-3116e258dea3

---
+++ src/FileEncDec.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+from Decrypter import Decrypter
+from Encrypter import Encrypter
+import os , sys
+
+__all__ = ['FileNotSetException' , 'PassNotSetException' , 
+           'InvalidFileException' , 'FileEncDec']
+__version__ = '$Id$'
+
+class FileNotSetException (Exception): pass
+class PassNotSetException (Exception): pass
+class InvalidFileException (Exception): pass
+
+class FileEncDec (Decrypter , Encrypter):
+    
+    def __init__ (self , password=None , file=None):
+        self.setInFile(file)
+        Encrypter.__init__(self , password , None)
+        Decrypter.__init__(self , password , None)
+            
+    def setInFile (self , file):
+        """
+        Set the intput file object
+        """
+        if file == None:
+            return False
+        oFile = self._getFObject(file)
+        if oFile == None:
+            return False
+        
+        self.oFile = oFile
+        self.fdFile = oFile.fileno()
+        return True
+    
+    def getInFileObject (self):
+        """
+        Returns the input file object
+        """
+        return self.oFile
+    
+    def _getFObject (self , file , mode='r'):
+        if file == None:
+            raise InvalidFileException , 'You must specify a file'
+        # 'file' can be either a file object, file descriptor, or path to a file
+        if str(type(file)).find('file') > -1:
+            if not (file.mode.find(mode) > -1 or file.mode.find('+')):
+                file.mode = mode
+            return file
+        elif type(file) == type(1):
+            # We supposedly have a file descriptor, it's going to throw an
+            # exception if the int is not actually a file desciptor
+            try:
+                fObject = os.fdopen(file)
+            except OSError:
+                raise InvalidFileException , 'The integer does not refer ' + \
+                                             'to a valid file descriptor'
+            if not (fObject.mode.find(mode) > -1 or fObject.mode.find('+')):
+                fObject.mode = mode
+            return fObject
+        elif type(file) == type('string'):
+            try:
+                fObject = open(file , mode)
+            except IOError:
+                err = 'The filename, %s, is not readable' % file
+                raise InvalidFileException , err
+            return fObject
+        
+        return None
+    
+    def _encDecFile (self , outFile , op):
+        if self.oFile == None:
+            raise FileNotSetException , 'You must set a file to be en/decrypted'
+        if self.pwHash == None:
+            raise PassNotSetException , 'You must set a password to ' + \
+                                        'en/decrypt the file'
+        try:
+            oOutFile = self._getFObject(outFile , 'w')
+        except InvalidFileException:
+            # Default to standard out if the outfile is invalid
+            oOutFile = sys.stdout
+            
+        while True:
+            block = self.oFile.read(4096)
+            if not block:
+                break
+            oOutFile.write(op(block))
+            oOutFile.flush()
+        oOutFile.close()
+        
+    def encFile (self , outFile):
+        """
+        You must pass in either a filename, a file handle or a valid file
+        descriptor (such as sys.stdout) to write the encryption output to.
+        If a valid file is NOT passed in, the output will default to stdout
+        """
+        self._encDecFile(outFile, self.encrypt)
+
+    def decFile (self , outFile):
+        """
+        You must pass in either a filename, a file handle or a valid file
+        descriptor (such as sys.stdout) to write the decryption output to.
+        If a valid file is NOT passed in, the output will default to stdout
+        """
+        self._encDecFile(outFile , self.decrypt)
+    def close (self):
+        if not self.oFile.closed:
+            self.oFile.close()
+        
+# Testing
+if __name__ == '__main__':
+    if sys.argv[1] == 'enc':
+        fenc = FileEncDec('chicken' , '/home/jdeiman/writetest')
+        fenc.encFile('/home/jdeiman/encfile')
+    elif sys.argv[1] == 'dec':
+        fenc = FileEncDec('chicken' , '/home/jdeiman/encfile')
+        fenc.decFile(sys.stdout)
+    fenc.close()
+    
