8ad6fb199a031728a8b2f1d021e37b8a2bcac93f
jay <jay@2c8e1322-2329-47d6-85ce-3116e258dea3>
2007-03-14 17:48:17 +0000

    setup script for pwenc install
    git-svn-id: https://svn.splitstreams.com:444/pwenc/trunk@31 2c8e1322-2329-47d6-85ce-3116e258dea3

---
+++ src/setup.py
@@ -0,0 +1,31 @@
+import sys
+assert sys.version >= '2' , 'Install Python 2.0 or greater'
+
+try:
+    import Crypto.Hash
+except:
+    print """You need the python Crypto module to use this package.
+
+You can download this package at: 
+    http://www.amk.ca/files/python/crypto/pycrypto-2.0.1.tar.gz"""
+    sys.exit(1)
+    
+from distutils.core import setup
+
+# $Id$
+
+def runSetup ():
+    setup(name = 'pwenc',
+          version = '0.1 Alpha',
+          author = 'Jay Deiman' ,
+          author_email = 'jay@splitstreams.com' ,
+          url = 'http://splitstreams.com' ,
+          license = 'GPLv2' ,
+          platforms = [ 'unix' ] ,
+          description = 'Password file encrypter and decrypter' ,
+          packages = ['FileEncrypter'] ,
+          scripts = ['pwenc.py']
+    )
+    
+if __name__ == '__main__':
+    runSetup()
