e36301ea9bf4d9e67679ab1e27730e7bb94cbdff
Jay Deiman <jay@splitstreams.com>
2015-09-20 14:35:33 -0700

    Finished the encrypt/decrypt routines

--- setup.py
+++ setup.py
@@ -1,31 +1,14 @@
-import sys
-assert sys.version >= '2' , 'Install Python 2.0 or greater'
-
-try:
-    import Crypto.Hash
-except:
-    print """You need the python Crypto module to use this package.
-
-You can download this package at: 
-    http://www.amk.ca/files/python/crypto/pycrypto-2.0.1.tar.gz"""
-    sys.exit(1)
     
 from distutils.core import setup
 
-# $Id$
-
-def runSetup ():
-    setup(name = 'pwenc',
-          version = '0.1.1',
-          author = 'Jay Deiman' ,
-          author_email = 'jay@splitstreams.com' ,
-          url = 'http://splitstreams.com' ,
-          license = 'GPLv2' ,
-          platforms = [ 'unix' ] ,
-          description = 'Password file encrypter and decrypter' ,
-          packages = ['FileEncrypter'] ,
-          scripts = ['pwenc.py']
-    )
-    
-if __name__ == '__main__':
-    runSetup()
+setup(name='pwenc',
+    version='0.2.0',
+    author='Jay Deiman' ,
+    author_email='admin@splitstreams.com' ,
+    url='http://splitstreams.com' ,
+    license='GPLv2' ,
+    platforms=['unix'] ,
+    install_requires=['pycrypto>=2.0', 'six>=1.9.0']
+    description='Password file encrypter and decrypter' ,
+    scripts=['pwenc.py']
+)
